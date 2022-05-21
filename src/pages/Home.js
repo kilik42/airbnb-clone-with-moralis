e7083298d42ab50588224eb61055f24871131705
marvin evins <marvinevins@yahoo.com>
2022-05-21 16:35:16 -0500

    working on the homepage

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,10 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
-import {bg} from "../images/frontpagebg.png";
+import bg from "../images/frontpagebg.png";
+// import {bg} from "../images/frontpagebg"
 import logo from "../images/airbnb.png";
+import { ConnectButton } from "web3uikit";
 
 const Home = () => {
 
@@ -17,6 +19,25 @@ const Home = () => {
 
       </div>
 
+      <div className="topBanner">
+        <div>
+          <img className="logo" src={logo} alt="" />
+        </div>
+        <div className="tabs">
+          <div className="selected">places to stay</div>
+          <div>experiences</div>
+          <div>online experiences</div>
+        </div>
+        <div className="lrContainers">
+          <ConnectButton />
+        </div>
+      </div>
+
+    <div className="tabContent">
+      <div className="searchFields">
+        
+      </div>
+    </div>
     </>
   );
 };

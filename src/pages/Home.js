24d5be0ed54562113a64fcaadd0df114fd3f85df
marvin evins <marvinevins@yahoo.com>
2022-05-21 12:13:17 -0500

    working on home section

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,12 +1,22 @@
 import React from "react";
 import "./Home.css";
 import { Link } from "react-router-dom";
+import {bg} from "../images/frontpagebg.png";
+import logo from "../images/airbnb.png";
 
 const Home = () => {
 
   return (
     <>
-      <Link to="/rentals"> Rentals </Link>
+      {/* <Link to="/rentals"> Rentals </Link> */}
+
+      <div className="container" style ={{backgroundImage: `url(${bg})`}}>
+        <div className="containterGradient">
+          
+        </div>
+
+      </div>
+
     </>
   );
 };

f9f0aa1cc95a813cb92f1046ac9416167cb52a54
marvin evins <marvinevins@yahoo.com>
2022-05-21 16:48:04 -0500

    working on home js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import bg from "../images/frontpagebg.png";
 // import {bg} from "../images/frontpagebg"
 import logo from "../images/airbnb.png";
-import { ConnectButton } from "web3uikit";
+import { ConnectButton, Select } from "web3uikit";
 
 const Home = () => {
 
@@ -35,6 +35,52 @@ const Home = () => {
 
     <div className="tabContent">
       <div className="searchFields">
+        <div className="inputs">
+          location
+          <Select 
+            defaultOptionIndex={0}
+            onChange={(data) => console.log(data.label)}
+            options={[
+
+              {
+                id: "ny",
+                label:"new york"
+              },
+              {
+                id:"chi",
+                label:"chicago"
+              },
+              {
+                id: "db",
+                label:"Dubai"
+              },
+              {
+                id: "la",
+                label:"los angelos"
+              }
+            ]}
+           />
+        </div>
+        <div className="vl"/>
+
+        
+
+        <div className="inputs">
+          check in 
+        </div>
+        <div className="vl"/>
+          
+
+
+        <div className="inputs">
+          check out
+        </div>
+        <div className="vl"/>
+          
+  
+        <div className="inputs">
+          Guests
+        </div>
         
       </div>
     </div>
